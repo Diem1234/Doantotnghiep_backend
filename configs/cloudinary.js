6daf07af728235601c68154f9147baddfd4906ce
Diem1234 <diemnguyen.16032002dmx@gmail.com>
2024-06-27 09:34:43 +0700

    'u'

--- configs/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-    // Configuration
-cloudinary.config({ 
-        cloud_name: 'dtd9zqkar', 
-        api_key: '545728927133443', 
-        api_secret: '<-gxkDn1KYxeSe99882uwsrNC7vM>' // Click 'View Credentials' below to copy your API secret
-    });
-module.exports = cloudinary
